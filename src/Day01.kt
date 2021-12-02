c7066f6510dbc8ac1b3efdaca77681820a0e15b5
Kosernik <kornilovsergei28@gmail.com>
2021-12-02 16:24:01 +0300

    Day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,43 @@
 fun main() {
+
+    fun countIncreases(input: List<String>) :Int {
+        var result = 0
+
+        var prev :Int = input[0].toInt()
+
+        for (depth in input) {
+            if (depth.toInt() > prev) result++
+            prev = depth.toInt()
+        }
+
+        return result
+    }
+
+    fun countIncreasesThree(input: List<String>) :Int {
+        var result = 0
+
+        var prev :Int = input[0].toInt() + input[1].toInt() + input[2].toInt()
+
+        for (i in 2..(input.size-2)) {
+            val sum :Int = input[i-1].toInt() + input[i].toInt() + input[i+1].toInt()
+            if (sum > prev) result++
+            prev = sum
+        }
+
+        return result
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return countIncreases(input)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return countIncreasesThree(input)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
